ce28fa9045123be3160a7f50b9bbc0cf5832df5a
pablo.castro <pablo.castro@csa.es>
2025-09-04 14:36:08 +0200

    Added Name to Competition Started controller and view for competition

--- Formula2/Controllers/CompetitionController.cs
+++ Formula2/Controllers/CompetitionController.cs
@@ -1,10 +1,28 @@
 using System.Diagnostics;
+using DataAccess.Data;
+using DataAccess.Repository;
+using DataAccess.Repository.IRepository;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Formula2.Controllers
 {
     public class CompetitionController : Controller
     {
-        
+        private readonly IUnitOfWork _db;
+
+        public CompetitionController(IUnitOfWork unitOfWork)
+        {
+            _db = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Upsert()
+        {
+            return View();
+        }
     }
 }
--- Models/Competition.cs
+++ Models/Competition.cs
@@ -9,18 +9,21 @@ namespace Models
         [Column("ID_COMPETITION"), Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
-        //public List<Race> Races { get; set; }
+        [Column("NAME")]
+        public string Name { get; set; }
+
         [Column("DESCRIPTION")]
         public string? Description { get; set; }
 
         [Column("PLAYOFFS_ON_CHAMPIONSHIP")]
         public bool Playoffs { get; set; }
 
+        [Column("CARS_CATEGORY")]
+        public Category? Category { get; set; }
+
+        //public List<Race> Races { get; set; }
         //public List<Race> PlayoffRaces { get; set; }
         //public List<Team> Teams { get; set; }
         //public List<Driver> Drivers { get; set; }
-
-        [Column("CARS_CATEGORY")]
-        public Category? Category { get; set; }
     }
 }
